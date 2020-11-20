ba3daa7b52a53ea070f6d18651e641cd1109c771
Chris81370 <christophe.molinier@cimpa.com>
2020-11-20 11:05:44 +0100

    ajout classes de test Junit

---
+++ java/TestJunitBerengere.java
@@ -0,0 +1,14 @@
+package pack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestJunitChristophe {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
